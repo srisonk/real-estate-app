c5e8ede2c21d1fb11d0db26e57b2dd6c369b118f
Srison Kadariya <a37444@alunos.ipb.pt>
2020-10-16 13:32:41 +0100

    Contacts with email application added

--- backend/realest_estate/settings.py
+++ backend/realest_estate/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'accounts',
     'realtors',
     'listings',
+    'contacts',
 ]
 
 MIDDLEWARE = [
@@ -89,6 +90,13 @@ DATABASES = {
         'HOST': 'localhost'
     }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'hhacked789@gmail.com'
+EMAIL_HOST_PASSWORD = 'tplrqhwakbwrnqnh'
+EMAIL_USE_TLS = True
 
 
 # Password validation
