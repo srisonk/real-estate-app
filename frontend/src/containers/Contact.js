7d6365d62393c2fdbadd4ebac2e098442b9ba733
Srison Kadariya <a37444@alunos.ipb.pt>
2020-10-19 21:25:42 +0100

    Front-end structure added

---
+++ frontend/src/containers/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const contact = () => (
+    <div>
+        contact
+    </div>
+);
+
+export default contact;
---
+++ frontend/src/containers/SignUp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const signUp = () => (
+    <div>
+        sign up
+    </div>
+);
+
+export default signUp;
